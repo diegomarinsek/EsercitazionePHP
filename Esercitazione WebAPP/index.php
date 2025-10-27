48f93107ba5392dbef89d437ea39135c5e4f5fe3
diegomarinsek <marinseksamuel@gmail.com>
2025-10-27 12:33:45 +0100

    d

---
+++ Esercitazione WebAPP/index.php
@@ -0,0 +1,7 @@
+<?php
+    include 'conf/config.php'; // path e il nome del file da includere
+    if(isset ($name))
+        echo "<h1>$name</h1>";
+    else
+        echo "<h1>Impossibile accedere al file config.php</h1>"
+?>
