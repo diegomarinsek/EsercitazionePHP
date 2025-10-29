5363e6dd526e88499007de7e8ba33ce46397ef2b
diegomarinsek <marinseksamuel@gmail.com>
2025-10-29 13:17:26 +0100

    Update index.php

--- EsercitazioneWebAPP/Calcolatrice/index.php
+++ EsercitazioneWebAPP/Calcolatrice/index.php
@@ -7,8 +7,17 @@ $risultato= "";
         $operazione = $_POST["operazione"];
         
         if($operazione == "somma"){
-            include "funzioni/somma.php"
-            $risultato = somma()
+            include "funzioni/somma.php";
+            $risultato = somma($num1,$num2);
+        }elseif ($operazione == "sottrazione"){
+            include "funzioni/sottrazione.php";
+            $risultato = sottrazione($num1,$num2);
+        }elseif ($operazione == "divisione"){
+            include "funzioni/divisione.php";
+             $risultato = divisione($num1,$num2);
+        }elseif ($operazione == "moltiplicazione"){
+            include "funzioni/moltiplicazione.php";
+             $risultato = moltiplicazione($num1,$num2);
         }
     
         
