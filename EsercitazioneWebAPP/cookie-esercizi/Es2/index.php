b0e9387b4bae470f1e6bd7120ac3b7f90a89a70e
PvToSeK <marinseksamuel@gmail.com>
2025-11-10 13:42:16 +0100

    ok

---
+++ EsercitazioneWebAPP/cookie-esercizi/Es2/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Esercizio 2 cookie</title>
+</head>
+<body>
+    <label for= nome>Inserire nome</label>
+    <input type="text" name = "nome">
+    <label for= cognome>Inserire cognome</label>
+    <input type="text" name = "cognome"> 
+    <button type = "submit">Invia</button>
+</body>
+</html>
+
+<?php
+
+
+?>
