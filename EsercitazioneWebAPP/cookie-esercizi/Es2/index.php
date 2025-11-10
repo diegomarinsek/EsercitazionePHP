28f23fe4d10fa1c13e48bcfbc3388e7b675a003f
diegomarinsek <marinseksamuel@gmail.com>
2025-11-10 21:59:14 +0100

    s

--- EsercitazioneWebAPP/cookie-esercizi/Es2/index.php
+++ EsercitazioneWebAPP/cookie-esercizi/Es2/index.php
@@ -6,6 +6,7 @@
     <title>Esercizio 2 cookie</title>
 </head>
 <body>
+    <form action="personalizzazione.php" method = POST>
     <label for= "nome">Inserire nome</label>
     <input type="text" name = "nome">
     <label for= "cognome">Inserire cognome</label>
@@ -16,8 +17,8 @@
     <input type="color" name = "coloreTesto">
     <label for="coloreSfondo">Inserire colore sfondo </label>
     <input type="color" name = "coloreSfondo">
-    
     <button type = "submit">Invia</button>
+    </form>
 </body>
 </html>
 
---
+++ EsercitazioneWebAPP/cookie-esercizi/Es2/personalizzazione.php
@@ -0,0 +1,8 @@
+<?php
+$nome = $_POST["nome"];
+$cognome = $_POST["cognome"];
+$inputfont = $_POST["inputfont"];
+$coloreTesto= = $_POST["coloreTesto"];
+$coloreSfondo= $_POST["coloreSfondo"];
+
+?>
