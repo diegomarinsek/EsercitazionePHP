9019c910dfa0ad5301641039c742673cab74c16f
diegomarinsek <marinseksamuel@gmail.com>
2025-11-10 21:53:11 +0100

    si

--- EsercitazioneWebAPP/cookie-esercizi/Es2/index.php
+++ EsercitazioneWebAPP/cookie-esercizi/Es2/index.php
@@ -6,10 +6,17 @@
     <title>Esercizio 2 cookie</title>
 </head>
 <body>
-    <label for= nome>Inserire nome</label>
+    <label for= "nome">Inserire nome</label>
     <input type="text" name = "nome">
-    <label for= cognome>Inserire cognome</label>
+    <label for= "cognome">Inserire cognome</label>
     <input type="text" name = "cognome"> 
+    <label for="inputfont">Inserire font desiderato</label>
+    <input type="text" name = "inputfont">
+    <label for="coloreTesto">Inserire colore font </label>
+    <input type="color" name = "coloreTesto">
+    <label for="coloreSfondo">Inserire colore sfondo </label>
+    <input type="color" name = "coloreSfondo">
+    
     <button type = "submit">Invia</button>
 </body>
 </html>
