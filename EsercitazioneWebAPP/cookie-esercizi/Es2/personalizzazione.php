64895d4567b5c5389d7e3712bb66f2e450031a74
PvToSeK <marinseksamuel@gmail.com>
2025-11-12 13:37:04 +0100

    Update personalizzazione.php

--- EsercitazioneWebAPP/cookie-esercizi/Es2/personalizzazione.php
+++ EsercitazioneWebAPP/cookie-esercizi/Es2/personalizzazione.php
@@ -7,5 +7,20 @@ $coloreSfondo= $_POST["coloreSfondo"];
 
 
 $valori =($nome,$cognome,$inputfont,$coloreTesto,$coloreSfondo);
-setcookie("preferenze",$)
+setcookie("preferenze",$valori, time()+200);
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Scheda personalizzata</title>
+</head>
+<body style="background-color: <?php echo $coloreSfondo; ?>; color: <?php echo $coloreTesto; ?>; font-family: <?php echo $inputfont; ?>;">
+ <?php
+        echo "<p>Bentornato $nome $cognome</p>";
+        echo '<p><a href="index.php">Torna alla home</a></p>';
+    ?>
+    
+</body>
+</html>
